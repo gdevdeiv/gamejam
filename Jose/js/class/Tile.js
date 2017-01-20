4cb276c5e1aa856f64b07b1917c233eb555ce41b
Ivan <dyfrok@gmail.com>
2017-01-20 23:58:16 +0100

    xd

--- Jose/js/class/Tile.js
+++ Jose/js/class/Tile.js
@@ -2,4 +2,13 @@ var Tile = function(type,x,y) {
     this.type = type;
     this.x = x;
     this.y = y;
+    if(this.type === "white"){this.img = "img/tiles/white.png";}
+    if(this.type === "black"){this.img = "img/tiles/black.png";}
+    if(this.type === "red"){this.img = "img/tiles/red.png";}
+    if(this.type === "green"){this.img = "img/tiles/green.png";}
+    if(this.type === "blue"){this.img = "img/tiles/blue.png";}
+    if(this.type === "yellow"){this.img = "img/tiles/yellow.png";}
+    if(this.type === "cyan"){this.img = "img/tiles/cyan.png";}
+    if(this.type === "magenta"){this.img = "img/tiles/magenta.png";}
+    
 }
