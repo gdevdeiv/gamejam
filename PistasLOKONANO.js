7cb4f23614aadc8d553000edf10cac9daf96c0f0
Ivan <dyfrok@gmail.com>
2017-01-21 20:27:55 +0100

    fge

---
+++ PistasLOKONANO.js
@@ -0,0 +1,59 @@
+/* 
+MANZANA -1 / HACHA -2 / BOTAS -3 / CARNE -4 / VESTIMENTA - 5 // BARRO - 6
+PICO -7 / PIEDRA -8 / PALA 9 / PIEL -10 / MADERA -11 
+*/
+function Pistas(){
+
+    var eventNum = [];
+    eventNum[0] = null;
+    eventNum[1] = null;
+
+    this.txt = [];
+
+    this.txt[0] = "Hoy vas a tener suerte";
+
+    this.txt[1] = "Deberías buscar alimento para sobrevivir."; 
+    this.txt[2] = "Te sientes hambriento.";
+
+    this.txt[] = "Parece que están llegando nuves..."; 
+
+
+    this.txt[] = "Bajarán las temperaturas drásticamente."; 
+    this.txt[] = "Tienes frío."; 
+
+    this.txt[] = "Hay animales salvajes cerca, podrían atacarte.";
+    this.txt[] = "Has visto pisadas de osos cerca de la casa."; //
+    this.txt[] = "Se escuchan aullidos de lobo bastante cercanos."; //  
+
+    this.txt[] = ""; // 
+    this.txt[] = ""; // 
+    this.txt[] = "Deberías buscar alimento para sobrevivir."; // 
+    this.txt[] = "Deberías buscar alimento para sobrevivir."; // 
+    this.txt[] = "Deberías buscar alimento para sobrevivir."; // 
+    this.txt[] = "Deberías buscar alimento para sobrevivir."; // 
+    this.txt[] = "Deberías buscar alimento para sobrevivir."; // 
+    this.txt[] = "Deberías buscar alimento para sobrevivir."; // 
+    this.txt[] = "Deberías buscar alimento para sobrevivir."; // 
+    this.txt[] = "Deberías buscar alimento para sobrevivir."; // 
+
+    this.creaPista = function(){
+        eventNum[0] = Math.floor(Math.random()*this.txt.length-1);
+        do{
+            eventNum[1] = Math.floor(Math.random()*this.txt.length-1);
+        }while(eventNum[0] == eventNum[1]);
+        
+        Hud.announceRound();
+    }
+
+    this.exe = function(){
+        var suerte = false;
+        if(eventNum[0] == 0 || eventNum[1] == 0){
+
+        }else{
+
+        }
+    }
+}
+
+var pistas = new Pistas();
+
