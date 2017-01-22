9613926b7b246536137db74455247537a70cbdf2
David Juan Ahullana <info@davidjotta.me>
2017-01-22 12:35:35 +0100

    Eventos.

--- js/core/Round.js
+++ js/core/Round.js
@@ -1,6 +1,6 @@
 var Round = function(round, game) {
 	this.round = round;
-	this.lastRound = 0;
+	this.lastRound = -1;
 	this.remaining = null;
 	this.duration = 30;
 	this.roundTime = this.duration * 1000;
@@ -9,19 +9,19 @@ var Round = function(round, game) {
 	this.minute = null;
 	this.started = false;
 	this.tips = [
-		"Hoy vas a tener suerte",
-		"Deberías buscar alimento para sobrevivir.",
-		"Te sientes hambriento.",
-		"Parece que están llegando nuves...",
-		"Parece que están llegando nuves oscuras.",
-		"Bajarán las temperaturas drásticamente.",
-		"Tienes frío.",
-		"Hay animales salvajes cerca, podrían atacarte.",
-		"Has visto pisadas de osos cerca de la casa.",
-		"Se escuchan aullidos de lobo bastante cercanos.",
-		"ha llovido esta noche y el suelo esta embarrado",
-		"ha nevado esta noche y ahora esta todo nevado"
-	]
+		{ msg: "Hoy vas a tener suerte.", event: null },
+		{ msg: "Deberías buscar alimento para sobrevivir.", event: { food: 2 } },
+		{ msg: "Te sientes hambriento.", event: { food: 1 } },
+		{ msg: "Parece que están llegando nubes.", event: { leather: 1 } },
+		{ msg: "Parece que están llegando nubes oscuras.", event: { leather: 2 } },
+		{ msg: "Bajarán las temperaturas drásticamente.", event: { wood: 1 } },
+		{ msg: "Tienes frío.", event: { coat: 1 } },
+		{ msg: "Hay animales salvajes cerca, podrían atacarte.", event: { axe: 1 } },
+		{ msg: "Has visto pisadas de osos cerca de la casa.", event: { axe: 1 } },
+		{ msg: "Se escuchan aullidos de lobo bastante cercanos.", event: { axe: 1 } },
+		{ msg: "Ha llovido esta noche y el suelo esta embarrado.", event: { boots: 1 } },
+		{ msg: "Ha nevado esta noche y ahora esta todo nevado.", event: { shovel: 1 } }
+	];
 }
 
 Round.prototype.start = function () {
